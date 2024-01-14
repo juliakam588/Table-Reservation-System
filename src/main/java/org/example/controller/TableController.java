3902d6b7a3289eca267123c6c9cf8733423a2808
Julia Kamuda <julka.kamuda@gmail.com>
2024-01-14 23:52:19 +0100

    updated controller and service layer for table

--- src/main/java/org/example/controller/TableController.java
+++ src/main/java/org/example/controller/TableController.java
@@ -4,6 +4,8 @@ import org.example.model.TableService;
 import org.example.model.entities.Table;
 import org.example.view.TableView;
 
+import java.util.List;
+
 public class TableController {
     private TableService tableService;
     private TableView tableView;
@@ -15,6 +17,50 @@ public class TableController {
     }
 
 
+    public void addTable(int capacity) {
+        try {
+            tableService.addTable(capacity);
+            System.out.println("Table with capacity " + capacity + " added successfully to the database.");
 
+        } catch (Exception e) {
+            System.out.println("Failed to add table: " + e.getMessage());
+        }
+    }
+
+    public void deleteTable(int tableId) {
+        try {
+            tableService.deleteTable(tableId);
+            System.out.println("Table with ID " + tableId + " deleted successfully.");
+        } catch (Exception e) {
+            System.out.println("Failed to delete table: " + e.getMessage());
+        }
+    }
+
+    public void showAvailableTables() {
+        try {
+            List<Table> availableTables = tableService.getAvailableTables();
+            if (!availableTables.isEmpty()) {
+                tableView.displayMainRow();
+                for (Table table : availableTables) {
+                    tableView.displayTable(table.getId(), table.getCapacity(), table.isAvailable());
+                }
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void showAllTables() {
+        try {
+            List<Table> allTables = tableService.getAllTables();
+            if (!allTables.isEmpty()) {
+                tableView.displayMainRow();
+                for (Table table : allTables) {
+                    tableView.displayTable(table.getId(), table.getCapacity(), table.isAvailable());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to display all tables: " + e.getMessage());
+        }
+    }
 
 }
--- src/main/java/org/example/model/TableService.java
+++ src/main/java/org/example/model/TableService.java
@@ -1,11 +1,11 @@
 package org.example.model;
 
-import org.example.model.dao.ReservationDAO;
 import org.example.model.dao.TableDAO;
-import org.example.model.entities.Reservation;
 import org.example.model.entities.Table;
 
 import java.util.List;
+
+import static org.example.model.entities.Table.MAX_TABLE_CAPACITY;
 
 public class TableService {
     private TableDAO tableDAO;
@@ -14,17 +14,48 @@ public class TableService {
         this.tableDAO = tableDAO;
     }
 
-    public void addReservation(Table table) {
-        tableDAO.insertTable(table);
+    public void addTable(int tableCapacity) {
+        if (tableCapacity <= 0 || tableCapacity > MAX_TABLE_CAPACITY) {
+            System.out.println("Invalid table capacity. It must be a positive number up to " + MAX_TABLE_CAPACITY + ".");
+            return;
+        }
+        Table table = new Table();
+        table.setCapacity(tableCapacity);
+        tableDAO.insert(table);
     }
 
-    public void deleteReservation(int tableID) {
-        tableDAO.deleteTable(tableID);
+    public void deleteTable(int tableId) {
+        Table tableToDelete = getTableById(tableId);
+        if (tableToDelete == null) {
+            System.out.println("Table with ID " + tableId + " does not exist.");
+            return;
+        }
+        if (!isTableAvailable(tableId)) {
+            System.out.println("Table with ID " + tableId + " is currently reserved and cannot be deleted.");
+            return;
+        }
+        tableDAO.delete(tableId);
     }
 
-    public List<Table> getAllCustomers() {
-        return tableDAO.getAllTables();
+    public List<Table> getAvailableTables() {
+        List<Table> availableTables = tableDAO.getAvailableTables();
+        if(availableTables.isEmpty()) {
+            System.out.println("No available tables at the moment.");
+            return null;
+        }
+        return availableTables;
     }
 
+    public List<Table> getAllTables() {
+        return tableDAO.getAll();
+    }
 
+    public Table getTableById(int tableId) {
+        return tableDAO.get(tableId);
+    }
+
+    public boolean isTableAvailable(int tableId) {
+        Table table = getTableById(tableId);
+        return table != null && table.isAvailable();
+    }
 }
