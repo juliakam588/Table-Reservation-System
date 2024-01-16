62324abb5e2af3d63914c0ffb00b8f9a1b32d8ef
Julia Kamuda <julka.kamuda@gmail.com>
2024-01-16 01:20:00 +0100

    deleted

--- src/main/java/org/example/strategy/SortingStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.example.strategy;
-
-import org.example.model.entities.Reservation;
-
-import java.util.List;
-
-public interface SortingStrategy {
-    void executeSorting(List<Reservation> reservations);
-}
